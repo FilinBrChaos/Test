51967a31a4fe2280e8a8cbd0095cecab200ec244
Lushn <myEmai.@gmail.com>
2020-12-16 14:51:59 +0200

    V1.2

--- Test progect for GitHub/Program.cs
+++ Test progect for GitHub/Program.cs
@@ -10,6 +10,8 @@ namespace Test_progect_for_GitHub
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("I made some changes again.");
+            Console.ReadKey();
         }
     }
 }
